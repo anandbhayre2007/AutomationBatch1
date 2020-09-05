575f2492e4109fe6e09fe57e3d1dbb0b568c447e
anandbhayre2007 <anandbhayre2007@gmail.com>
2020-09-05 14:06:40 +0530

    First time commiting on git

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,35 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		int age=32; //byte, short, long
+		String name="anand";
+		char gender='M';
+		boolean marritalStatus=true;
+		double height=5.8;
+		
+		
+		System.out.println(age);
+		
+		//Reserved keywords
+		//Difference between byte, short, int, long
+		//difference between double and float
+		//JDK, JRE and JVM
+		
+		//Its a class
+		
+		//Primitive and Non primitive data types
+		
+		//Primitive
+		//byte, short, int, long, char, double, float, boolean
+		
+		//Non primitive data types
+		//classes, interfaces, arrays, collections etc.
+		
+		
+		
+	}
+
+}
